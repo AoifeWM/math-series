6057788f193bcd27ee03997d453815de0cd48bf8
AoifeWM <aoifewmulligan@gmail.com>
2022-05-19 17:58:52 -0700

    added basic series functions and ensured they passed unit tests by importing and invoking functions from test_series.py

--- series.py
+++ series.py
@@ -1,11 +1,46 @@
-def fibonacci(n){
+import test_series
 
-}
 
-def lucas(n){
+def fibonacci(n):
+    prev = 0
+    curr = 1
+    x = 0
+    if isinstance(n, int) and n >= 0:
+        for i in range(n):
+            x = curr + prev
+            prev = curr
+            curr = x
+        return prev
+    else:
+        return False
 
-}
 
-def sum_series(n){
+def lucas(n):
+    prev = 2
+    curr = 1
+    x = 0
+    if isinstance(n, int) and n >= 0:
+        for i in range(n):
+            x = curr + prev
+            prev = curr
+            curr = x
+        return prev
+    else:
+        return False
 
-}
+
+def sum_series(n, prev=0, curr=1):
+    x = 0
+    if isinstance(n, int) and n >= 0:
+        for i in range(n):
+            x = curr + prev
+            prev = curr
+            curr = x
+        return prev
+    else:
+        return False
+
+
+test_series.fib_test(fibonacci)
+test_series.lucas_test(lucas)
+test_series.sum_series_test(sum_series)
